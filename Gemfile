032cc107ea5028d8d0085a1526e32eba79a2ba1b
romain despature <rdespature@gmail.com>
2017-05-18 13:10:31 +0200

    initial commit

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem 'rspec-rails', group: [ :test ]
+
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
--- rails-yelp-mvp/db/seeds.rb
+++ rails-yelp-mvp/db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "01 34 23 09 56",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "03 20 23 45 56",
+    category:     "belgian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "04 25 87 47 03",
+    category:     "chinese"
+  },
+  {
+    name:         "T'klopf",
+    address:      "bvd de la Louvière 8930 Rekkem",
+    phone_number: "04 25 87 47 03",
+    category:     "belgian"
+  },
+  {
+    name:         "Barbus d'Anvers",
+    address:      "place Nicolas II 59800 Lille",
+    phone_number: "03 25 12 78 41",
+    category:     "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
